23bca2bd4ae8a7493340c2e612603a04b3a8f229
IPADILHA <padilha.israelly@escola.pr.gov.br>
2025-05-22 20:56:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'História',
+    'A Segunda Grande Guerra adquiriu caráter mundial a partir de 7 de dezembro de 1941, quando?',
+    'Os japoneses atacaram a base norte-americana de Pearl Harbor.'
+)
+
+criaCartao(
+    'Biologia',
+    'Orgão sensorial das víboras?',
+    'Fosseta Loreal'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Lituânia?',
+    'Vilnius'
+)
+criaCartao(
+    'Sociologia',
+    'O que a socialogia estuda?',
+    'A sociedade'
+)
+criaCartao(
+    'Matematica',
+    'Geometria é a área da Matemática que trabalha com?',
+    'Sólidos, superfícies, linhas, pontos ângulos e suas relações'
+)
+criaCartao(
+    'Filosofia',
+    'Segundo Sartre, o que significa que o “homem está condenado a ser livre”?',
+    'Significa que todo ser humano é responsável por tudo que faz consigo e com os outross'
+)
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'É uma sequencia de instruções simples que tem como objetivo obter um resultado'
+)
+criaCartao(
+     'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
